59a6491263e7506cca6d52724a94268c78b706a4
Torben Möller <torben.moeller@gmx.net>
2018-08-13 00:17:11 +0200

    Finished

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,16 +1,10 @@
 package com.udacity.gradle.builditbigger;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
-
-import com.udacity.jokeactivity.JokeActivity;
-import com.udacity.jokeactivity.Keys;
-import com.udacity.jokes.Jokes;
 
 
 public class MainActivity extends AppCompatActivity {
